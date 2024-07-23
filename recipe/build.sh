f65daabd053ec37f4ba233d687c184882ab2ce9d
H. Vetinari <h.vetinari@gmx.com>
2024-07-23 16:25:25 +1100

    set --disable-dependency-tracking also on linux-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,13 +9,7 @@ elif [[ "$target_platform" == osx* ]]; then
   rm -f tests/scripts/variables/INCLUDE_DIRS
 fi
 
-if [[ ${target_platform} =~ .*aarch64.* || ${target_platform} =~ .*ppc64le.* ]]; then
-  config_opts="--disable-dependency-tracking"
-else
-  config_opts=""
-fi
-
-./configure --prefix=$PREFIX ${config_opts}
+./configure --prefix=$PREFIX --disable-dependency-tracking
 # bootstrap building make without make
 bash build.sh
 # make
